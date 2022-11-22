e27ea5a06eb1736c2c7447e6a5b65d504fbb176d
Maxime Poncet <maxime.poncet@gmail.com>
2022-11-22 21:49:07 +0100

    Remove mut from trait repository, prefer interior mutabulity

--- src/repository/inmemory.rs
+++ src/repository/inmemory.rs
@@ -1,38 +1,50 @@
+use std::cell::RefCell;
+
 use crate::device::{Device, MacAddress};
 use crate::repository::{DeleteError, DeviceRepository, InsertError};
 
 pub struct InMemoryDeviceRepository {
-    devices: Vec<Device>,
+    devices: RefCell<Vec<Device>>,
 }
 
 impl InMemoryDeviceRepository {
     pub fn new() -> Self {
-        Self { devices: vec![] }
+        Self {
+            devices: RefCell::new(vec![]),
+        }
     }
 }
 
 impl DeviceRepository for InMemoryDeviceRepository {
-    fn insert(&mut self, device: Device) -> Result<(), InsertError> {
-        if self.devices.iter().any(|d| d.name == device.name) {
+    fn insert(&self, device: Device) -> Result<(), InsertError> {
+        if self.devices.borrow().iter().any(|d| d.name == device.name) {
             Err(InsertError::Conflict)
         } else {
-            self.devices.push(device);
+            self.devices.borrow_mut().push(device);
             Ok(())
         }
     }
 
     fn fetch_by_name(&self, name: &str) -> Option<Device> {
-        self.devices.iter().find(|d| d.name == name).cloned()
+        self.devices
+            .borrow()
+            .iter()
+            .find(|d| d.name == name)
+            .cloned()
     }
 
     fn fetch_by_mac(&self, mac: &MacAddress) -> Option<Device> {
-        self.devices.iter().find(|d| d.mac == *mac).cloned()
+        self.devices
+            .borrow()
+            .iter()
+            .find(|d| d.mac == *mac)
+            .cloned()
     }
 
-    fn delete(&mut self, name: &str) -> Result<(), DeleteError> {
+    fn delete(&self, name: &str) -> Result<(), DeleteError> {
         let mut result = Err(DeleteError::NotFound);
 
-        self.devices.retain(|d| {
+        self.devices.borrow_mut().retain(|d| {
             if d.name == name {
                 result = Ok(());
                 false
@@ -45,10 +57,10 @@ impl DeviceRepository for InMemoryDeviceRepository {
     }
 
     fn fetch_all(&self) -> Option<Vec<Device>> {
-        if self.devices.is_empty() {
+        if self.devices.borrow().is_empty() {
             None
         } else {
-            Some(self.devices.clone())
+            Some(self.devices.borrow().clone())
         }
     }
 }
--- src/repository/mod.rs
+++ src/repository/mod.rs
@@ -11,8 +11,8 @@ pub enum DeleteError {
 }
 
 pub trait DeviceRepository {
-    fn insert(&mut self, device: Device) -> Result<(), InsertError>;
-    fn delete(&mut self, name: &str) -> Result<(), DeleteError>;
+    fn insert(&self, device: Device) -> Result<(), InsertError>;
+    fn delete(&self, name: &str) -> Result<(), DeleteError>;
     fn fetch_by_name(&self, name: &str) -> Option<Device>;
     fn fetch_by_mac(&self, mac: &MacAddress) -> Option<Device>;
     fn fetch_all(&self) -> Option<Vec<Device>>;
