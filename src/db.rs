532d7a8ccbb4254cd2f842d151b4dfce1bb26a09
Maxime Poncet <maxime.poncet@gmail.com>
2023-06-26 21:37:35 +0200

    Handle database errors

--- src/db.rs
+++ src/db.rs
@@ -9,10 +9,41 @@ pub struct Db {
     devices_by_user: HashMap<SlackUserId, MacAddress>,
 }
 
+#[derive(Debug)]
+pub enum DbError {
+    IoError(std::io::Error),
+    DeserializeError,
+}
+
+impl std::error::Error for DbError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match *self {
+            DbError::IoError(ref err) => Some(err),
+            DbError::DeserializeError => None,
+        }
+    }
+}
+
+impl std::fmt::Display for DbError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match *self {
+            DbError::IoError(ref err) => err.fmt(f),
+            DbError::DeserializeError => write!(f, "Deserialize error"),
+        }
+    }
+}
+
+impl From<std::io::Error> for DbError {
+    fn from(err: std::io::Error) -> Self {
+        DbError::IoError(err)
+    }
+}
+
 impl Db {
-    pub fn with_file<P: AsRef<Path>>(path: P) -> Result<Self, std::io::Error> {
+    pub fn with_file<P: AsRef<Path>>(path: P) -> Result<Self, DbError> {
         let raw_yaml = std::fs::read_to_string(path.as_ref())?;
-        let devices_by_user = serde_yaml::from_str(&raw_yaml).unwrap();
+        let devices_by_user =
+            serde_yaml::from_str(&raw_yaml).map_err(|_| DbError::DeserializeError)?;
 
         Ok(Self { devices_by_user })
     }
